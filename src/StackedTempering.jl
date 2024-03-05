021412a41b4d8329805f8b6e9033703c08cbe8f1
Jorge FdCD <jfdecd@icloud.com>
2024-03-05 14:14:08 +0100

    imports

--- src/StackedTempering.jl
+++ src/StackedTempering.jl
@@ -1,10 +1,14 @@
 module StackedTempering
 
-using Base: tail, front
-using RestrictedBoltzmannMachines: sample_v_from_h, sample_h_from_v, free_energy_h, free_energy_v
-using Tail2Front2: tail2
-using MetropolisRules: metropolis_rule
+using Base: front
+using Base: tail
 using ConditionalSwaps: swap
+using MetropolisRules: metropolis_rule
+using RestrictedBoltzmannMachines: free_energy_h
+using RestrictedBoltzmannMachines: free_energy_v
+using RestrictedBoltzmannMachines: sample_h_from_v
+using RestrictedBoltzmannMachines: sample_v_from_h
+using Tail2Front2: tail2
 
 include("stacked_tempering.jl")
 include("swap.jl")
